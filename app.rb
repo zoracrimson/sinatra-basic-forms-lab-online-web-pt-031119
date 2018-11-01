3be0dbc16877f88a8815182af3ecd21e26892b90
Graham Troyer-Joy <graham.troyer-joy@flatironschool.com>
2018-11-01 14:55:10 -0400

    actually use puppy instance

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ class App < Sinatra::Base
   end
 
   post '/puppy' do
-    @puppy = Puppy.new(params['name'], params['breed'], params['age'])
+    @puppy = Puppy.new(params['name'], params['breed'], params['months_old'])
     erb :display_puppy
   end
 
