e3a3a57bde5772b9f2e2784746556efe328cc36c
Graham Troyer-Joy <graham.troyer-joy@flatironschool.com>
2018-11-01 14:13:25 -0400

    removes solution code from controller

--- app.rb
+++ app.rb
@@ -2,21 +2,4 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-
-  get "/" do
-    erb :index
-  end
-
-
-  get "/new" do 
-    erb :create_puppy
-  end
-
-  post "/" do
-    @puppy = Puppy.new(params[:name], 
-                       params[:breed], 
-                        params[:months_old])
-    erb :display_puppy
-  end
-
 end
