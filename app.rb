a3130d5532b93ff2ae4ec191936cbf23d9ea8a5a
Graham Troyer-Joy <graham.troyer-joy@flatironschool.com>
2018-11-01 14:55:10 -0400

    actually use puppy instance

--- app.rb
+++ app.rb
@@ -2,4 +2,20 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+<<<<<<< HEAD
+=======
+  get '/' do
+    erb :index
+  end
+
+  get '/new' do
+    erb :create_puppy
+  end
+
+  post '/puppy' do
+    @puppy = Puppy.new(params['name'], params['breed'], params['months_old'])
+    erb :display_puppy
+  end
+
+>>>>>>> actually use puppy instance
 end
