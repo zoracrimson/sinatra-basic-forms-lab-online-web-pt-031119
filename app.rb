2d3e2fea94c50ed664279f284b0e72f40c82fc93
maxwellbenton <maxwellbenton@gmail.com>
2018-11-09 09:05:53 -0500

    reset starter code

--- app.rb
+++ app.rb
@@ -2,19 +2,4 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do
-    erb :index
-  end
-
-  get '/new' do
-    erb :create_puppy
-  end
-
-  post '/puppy' do
-    @name = params['name']
-    @age = params['age']
-    @breed = params['breed']
-    erb :display_puppy
-  end
-
 end
