dad1a8598a0f51d87fd04f22ba3c1b44613d515d
maxwellbenton <maxwellbenton@gmail.com>
2019-03-18 11:28:37 -0400

    fixes inconsistent tests

--- spec/models/puppy_spec.rb
+++ spec/models/puppy_spec.rb
@@ -26,13 +26,13 @@ describe 'Puppy class' do
     expect(puppy.breed).to eq("black lab")
   end
 
-  it 'can read a puppy age (puppy#age)' do 
-    expect(puppy.age).to eq(2)
+  it 'can read a puppy age in months (puppy#months_old)' do
+    expect(puppy.months_old).to eq(2)
   end
 
-  it 'can change puppy age (puppy#age=)' do 
-    puppy.age = 3
-    expect(puppy.age).to eq(3)
+  it 'can change puppy age in months (puppy#months_old=)' do 
+    puppy.months_old = 3
+    expect(puppy.months_old).to eq(3)
   end
 
   it 'can change puppy name' do
